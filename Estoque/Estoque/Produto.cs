8a0e760934bd74145dd33eaeb11ee4d2716a8013
LeonarDev <leonardo.majevski@gmail.com>
2021-08-02 16:22:05 -0300

    feat: product registration

---
+++ Estoque/Estoque/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Estoque {
+    class Produto {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString() {
+            return $"{Nome}, R$ {Preco.ToString("F2", CultureInfo.InvariantCulture)}, " +
+                $"{Quantidade} unidades, Total: R$ {ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- Estoque/Estoque/Program.cs
+++ Estoque/Estoque/Program.cs
@@ -3,7 +3,17 @@
 namespace Estoque {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do produto: {p}");
         }
     }
 }
